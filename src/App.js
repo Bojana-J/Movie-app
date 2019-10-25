e255e237957f0aef585b9d4475b258f1e543cb83
bojana <b.janjicic@gmail.com>
2019-10-25 13:42:33 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import {Route} from 'react-router-dom';
 import './App.css';
 import Movies from './components/movie';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import Customers from './components/customers';
 
 function App() {
   return (
     <main className='container' >
       
     <Movies></Movies>
-
-
+    <Route path="/movies" component={Movies} />
+    <Route path="/customers" component={Customers} />
+    <Route path="/rentals" component={Rentals} />
+    <Route path="/not-found" component={NotFound} />
     </main>
   );
 }
